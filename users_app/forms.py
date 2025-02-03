2e7878c72657537c7e99ef2bdd6c6a948cf234e6
Poojan26 <poojan.patel1126@gmail.com>
2025-02-03 18:18:57 -0500

    Updated form view with first and last name fields

--- users_app/forms.py
+++ users_app/forms.py
@@ -5,8 +5,10 @@ from django.contrib.auth.models import User
 
 class CustomRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.TextInput()
+    last_name = forms.TextInput()
     
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['username','email','first_name','last_name','password1','password2']
         
