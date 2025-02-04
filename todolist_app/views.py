715365384944572a28cd9b1bdc836af2c9ad5cf1
Poojan26 <poojan.patel1126@gmail.com>
2025-02-04 18:25:09 -0500

    Applied page restrictions for unauthorized users

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -4,8 +4,10 @@ from todolist_app.models import TaskList
 from todolist_app.forms import Taskform
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required 
 
 # Create your views here.
+@login_required
 def todolist(request):
     
     if request.method == "POST":
