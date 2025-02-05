f017e6d9ecbd053c71d1fda68ea3423cd77b7a7d
Poojan26 <poojan.patel1126@gmail.com>
2025-02-05 18:22:31 -0500

    csrf fix

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1","todo-list-production-616b.up.railway.app/"]
-CSRF_TRUSTED_ORIGINS = ['todo-list-production-616b.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://todo-list-production-616b.up.railway.app/']
 
 # Application definition
 
